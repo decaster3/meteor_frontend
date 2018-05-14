3c15e08b91e12ca3f162e5fb483704156d8840ca
bulnab <bulnab@gmail.com>
2018-05-14 11:35:11 +0300

    Move to SCSS modules

--- src/components/Pages/Main.tsx
+++ src/components/Pages/Main.tsx
@@ -1,132 +1,10 @@
+import * as classnames from "classnames"
 import * as React from "react"
 import styled from "styled-components"
 import bg from "./pattern_1.svg"
 
 // @ts-ignore
-import styles from "./Main.module.css"
-
-console.log("styles", styles)
-
-enum Color {
-  DarkBlue = "hsl(230, 60%, 5%)",
-  Blue = "hsl(226, 54%, 13%)",
-  Orange = "hsl(33, 99%, 50%)",
-  DarkGrey = "hsl(199, 20%, 15.5%)",
-  Grey = "hsl(200, 23%, 23.5%)",
-  LightGrey = "hsl(199, 28%, 31.5%)",
-}
-
-const StyledContainerWrapper = styled.div`
-  background-image: url(${bg});
-`
-
-const StyledBottomNavbar = styled.div`
-  display: flex;
-  background-color: ${Color.Orange};
-  justify-content: space-evenly;
-  align-items: center;
-  height: 5rem;
-  text-transform: uppercase;
-  font-weight: 700;
-  color: hsl(230, 60%, 5%, 0);
-  letter-spacing: 0.125rem;
-
-  > .separator {
-    height: 3rem;
-    width: 0.125rem;
-    background-color: ${Color.DarkBlue};
-  }
-`
-
-const StyledTopNavbar = styled.div`
-  display: flex;
-  background-color: ${Color.DarkBlue};
-  justify-content: space-evenly;
-  align-items: center;
-  height: 5rem;
-  text-transform: uppercase;
-  font-weight: 700;
-  color: white;
-  letter-spacing: 0.125rem;
-
-  > .separator {
-    height: 3rem;
-    width: 0.125rem;
-    background-color: ${Color.DarkGrey};
-  }
-`
-
-const StyledFooter = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-
-  > .separator {
-    width: 0.25rem;
-    height: 11rem;
-    background-color: ${Color.Grey};
-  }
-`
-
-const StyledMenu = styled.div`
-  padding: 0.5rem 2rem;
-  text-transform: uppercase;
-  color: white;
-  letter-spacing: 0.125rem;
-  font-weight: 500;
-
-  > div {
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-  }
-`
-
-const StyledSecondaryMenu = styled.div`
-  padding: 0.5rem 2rem;
-  color: white;
-  letter-spacing: 0.125rem;
-  font-weight: 500;
-
-  > div {
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-  }
-`
-
-const StyledLightLink = styled.a`
-  color: white;
-
-  &:hover,
-  &:focus {
-    color: ${Color.Orange};
-    text-decoration: none;
-  }
-`
-
-const StyledDarkLink = styled.a`
-  color: ${Color.DarkBlue};
-
-  &:hover,
-  &:focus {
-    color: white;
-    text-decoration: none;
-  }
-`
-
-const StyledBanner = styled.div`
-  color: ${Color.LightGrey};
-  border: 0.25rem solid ${Color.LightGrey};
-  text-transform: uppercase;
-  letter-spacing: 0.125em;
-  font-size: 2rem;
-  margin: 1rem;
-  padding: 1rem;
-  font-weight: 700;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-`
+import styles from "./Main.module.scss"
 
 const pages = ["City", "Menu", "Sales", "Profile", "Cart"]
 
@@ -147,84 +25,95 @@ const categories = [
 ]
 
 const Main = () => (
-  <StyledContainerWrapper className={styles["container-wrapper"]}>
+  <div className={styles.wrapper}>
     <div className={styles.backdrop}>
-      <div className={`container ${styles.container}`}>
-        <StyledTopNavbar className="row">
+      <div className={classnames("container", styles.container)}>
+        <div className={classnames(styles.topNavbar, "row")}>
           {pages.map((page, index) => (
             <React.Fragment key={index}>
-              {index > 0 && <div className="separator" />}
+              {index > 0 && <div className={styles.topNavbarSeparator} />}
               <div>
-                <StyledLightLink href="#">{page}</StyledLightLink>
+                <a href="#">{page}</a>
               </div>
             </React.Fragment>
           ))}
-        </StyledTopNavbar>
+        </div>
 
         <div className="row my-3">
           <div className="col-4">
-            <StyledBanner style={{height: "20rem"}}>Ad Banner</StyledBanner>
+            <div className={styles.banner} style={{height: "20rem"}}>
+              Ad Banner
+            </div>
           </div>
           <div className="col-8">
-            <StyledBanner style={{height: "20rem"}}>Image</StyledBanner>
+            <div className={styles.banner} style={{height: "20rem"}}>
+              Image
+            </div>
           </div>
         </div>
 
         <div className="row my-3">
           <div className="col">
-            <StyledBanner style={{height: "10rem"}}>How It Works</StyledBanner>
+            <div className={styles.banner} style={{height: "10rem"}}>
+              How It Works
+            </div>
           </div>
         </div>
 
-        <StyledBottomNavbar className="row">
+        <div className={classnames(styles.bottomNavbar, "row")}>
           {categories.map((category, index) => (
             <React.Fragment key={index}>
-              {index > 0 && <div className="separator" />}
+              {index > 0 && <div className={styles.bottomNavbarSeparator} />}
               <div>
-                <StyledDarkLink href="#">{category}</StyledDarkLink>
+                <a href="#">{category}</a>
               </div>
             </React.Fragment>
           ))}
-        </StyledBottomNavbar>
+        </div>
 
-        <StyledFooter className="row py-4 d-flex align-items-center">
-          <StyledMenu>
+        <div
+          className={classnames(
+            styles.footer,
+            "row py-4 d-flex align-items-center"
+          )}
+        >
+          <div className={styles.menu}>
             {categories.map(
               (category, index) =>
                 index < categories.length / 2 && (
                   <div key={index}>
-                    <StyledLightLink href="#">{category}</StyledLightLink>
+                    <a href="#">{category}</a>
                   </div>
                 )
             )}
-          </StyledMenu>
+          </div>
 
-          <div className="separator" />
+          <div className={styles.footerSeparator} />
 
-          <StyledMenu>
+          <div className={styles.menu}>
             {categories.map(
               (category, index) =>
                 index >= categories.length / 2 && (
                   <div key={index}>
-                    <StyledLightLink href="#">{category}</StyledLightLink>
+                    <a href="#">{category}</a>
                   </div>
                 )
             )}
-          </StyledMenu>
+          </div>
 
-          <div className="separator" />
+          <div className={styles.footerSeparator} />
 
-          <StyledSecondaryMenu>
+          <div className={styles.secondaryMenu}>
             {secondaryPages.map((secondaryPage, index) => (
               <div key={index}>
-                <StyledLightLink href="#">{secondaryPage}</StyledLightLink>
+                <a href="#">{secondaryPage}</a>
               </div>
             ))}
-          </StyledSecondaryMenu>
-        </StyledFooter>
+          </div>
+        </div>
       </div>
     </div>
-  </StyledContainerWrapper>
+  </div>
 )
 
 export default Main
