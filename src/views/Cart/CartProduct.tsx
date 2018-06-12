4940b438550c5201dc7db300c1345da233aa6055
bulnab <bulnab@gmail.com>
2018-06-12 03:01:31 +0300

    Modify cart product

--- src/views/Cart/CartProduct.tsx
+++ src/views/Cart/CartProduct.tsx
@@ -8,60 +8,65 @@ interface CartProductProps {
   addProductToCart(product: CartProduct): void
 }
 
+const handleAddProduct = (props: CartProductProps) => () => {
+  props.addProductToCart(props.product)
+}
+
+const handleRemoveProduct = (props: CartProductProps) => () => {
+  props.removeProductFromCart(props.product)
+}
+
 const CartProduct: React.StatelessComponent<CartProductProps> = props => {
-  const getOptions = (product: CartProduct) => {
-    let independentOptions = ""
-    let dependentOptions = ""
+  const independentOptions: Array<[string, string]> = []
+  const dependentOptions: Array<[string, string]> = []
 
-    product.instance.independentOptions.forEach(
-      (independentOption: OptionConcat) => {
-        product.options.forEach((option: Option) => {
-          if (option.id === independentOption.optionId) {
-            option.optionValues.forEach(
-              (optionValue: {value: string; id: number}) => {
-                if (optionValue.id === independentOption.valueId) {
-                  independentOptions += `${option.name} ${optionValue.value}`
-                }
-              }
-            )
+  props.product.instance.independentOptions.forEach(independentOption => {
+    props.product.options.forEach(option => {
+      if (option.id === independentOption.optionId) {
+        option.optionValues.forEach(optionValue => {
+          if (optionValue.id === independentOption.valueId) {
+            independentOptions.push([option.name, optionValue.value])
           }
         })
       }
-    )
+    })
+  })
 
-    product.instance.dependentOptions.forEach(
-      (dependentOption: OptionConcat) => {
-        product.options.forEach((option: Option) => {
-          if (option.id === dependentOption.optionId) {
-            option.optionValues.forEach(
-              (optionValue: {value: string; id: number}) => {
-                if (optionValue.id === dependentOption.valueId) {
-                  dependentOptions = `${dependentOptions} ${option.name} ${
-                    optionValue.value
-                  }`
-                }
-              }
-            )
+  props.product.instance.dependentOptions.forEach(dependentOption => {
+    props.product.options.forEach(option => {
+      if (option.id === dependentOption.optionId) {
+        option.optionValues.forEach(optionValue => {
+          if (optionValue.id === dependentOption.valueId) {
+            dependentOptions.push([option.name, optionValue.value])
           }
         })
       }
-    )
-    return `${independentOptions} ${dependentOptions}`
-  }
-  const handleAddProductClick = () => {
-    props.addProductToCart(props.product)
-  }
-  const handleRemoveProductClick = () => {
-    props.removeProductFromCart(props.product)
-  }
+    })
+  })
+
   return (
     <div>
       <div>{props.product.name}</div>
       <div>{props.product.description}</div>
       <div>{props.product.count}</div>
-      <div>{getOptions(props.product)}</div>
-      <button onClick={handleRemoveProductClick}>-</button>
-      <button onClick={handleAddProductClick}>+</button>
+      <div>
+        <ul>
+          {independentOptions.map(([name, value], index) => (
+            <li key={index}>
+              {name} {value}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {dependentOptions.map(([name, value], index) => (
+            <li key={index}>
+              {name} {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button onClick={handleRemoveProduct(props)}>-</button>
+      <button onClick={handleAddProduct(props)}>+</button>
     </div>
   )
 }
