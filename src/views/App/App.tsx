c9cdedb578fbfacc127ec4c3fbeac09b258f76b4
bulnab <bulnab@gmail.com>
2018-06-21 22:11:55 +0300

    Refactor App view styles

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,27 +1,52 @@
-import {ThemeProvider} from "emotion-theming"
+import {ThemeProvider, withTheme} from "emotion-theming"
 import * as React from "react"
 import {compose} from "redux"
 import {Switch, Route, withRouter} from "react-router-dom"
-
+import {injectGlobal, css} from "emotion"
 import {Container, Row} from "reactstrap"
+import * as Color from "color"
 
 import Menu from "../../views/MainContentPlaceholder/Menu"
 import MainPage from "../../views/MainPage"
 import Cart from "../../views/Cart"
-
 import {Status} from "../../constants"
 import {User} from "../../containers/UserSession/actions"
 import {UserState} from "../../containers/UserSession/constants"
 import Footer from "../Footer"
 import Header from "../Header"
-import * as styles from "./App.module.scss"
 import {theme} from "./Theme"
 import {Category} from "../../containers/Product/actions"
-
 import withGeolocation from "../../containers/Geolocation"
 import {withUser} from "../../containers/UserSession"
 import withCategories from "../../containers/Category"
 import {City} from "../../containers/Geolocation/actions"
+import pattern from "../../assets/pattern.png"
+
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  html,
+  body {
+    height: 100%;
+  }
+
+  body {
+    display: flex;
+    flex-flow: column;
+  }
+
+  #root {
+    flex: 1;
+    display: flex;
+    flex-flow: column;
+  }
+
+  html,
+  body {
+    background-size: 300px;
+    background-image: url("${pattern}");
+    background-color: ${theme.darkBlue};
+  }
+`
 
 interface AppProps {
   citiesStatus: Status
@@ -34,17 +59,55 @@ interface AppProps {
   categories: Category[]
 }
 
+const fadedDarkBlue = Color(theme.darkBlue)
+  .fade(0.5)
+  .hsl()
+  .string()
+
+const fadedBlue = Color(theme.blue)
+  .fade(0.5)
+  .hsl()
+  .string()
+
 const App: React.StatelessComponent<AppProps> = props => (
   <ThemeProvider theme={theme}>
-    <div className={styles.backdrop}>
+    <div
+      className={css`
+        flex: 1;
+        padding-top: 3.5rem;
+        background-color: ${fadedDarkBlue};
+        background: linear-gradient(
+          ${theme.darkBlue},
+          ${fadedDarkBlue},
+          ${theme.darkBlue}
+        );
+      `}
+    >
       <Header
         cities={props.cities}
         citiesStatus={props.citiesStatus}
         userInfo={props.userInfo}
         userState={props.userState}
       />
-      <Container className={styles.container}>
-        <Row className={styles.content}>
+      <Container
+        className={css`
+          display: flex;
+          flex-flow: column;
+          min-height: 100%;
+        `}
+      >
+        <Row
+          className={css`
+            flex: 1;
+            background-color: ${fadedBlue};
+            background: linear-gradient(
+              ${fadedBlue},
+              ${theme.blue},
+              ${fadedBlue}
+            );
+            box-shadow: 0 0 6rem 0 black, 0 0 2rem 0 black;
+          `}
+        >
           <Container fluid={true} className="py-3">
             <Switch>
               <Route
