031e67b9773e6c448422fea7b022f039b5312713
bulnab <bulnab@gmail.com>
2018-06-30 18:29:33 +0300

    Oprimize cart product view

--- src/views/Cart/CartProduct.tsx
+++ src/views/Cart/CartProduct.tsx
@@ -5,20 +5,6 @@ import Icon from "react-fa"
 import {CartProduct} from "../../containers/Cart/actions"
 import {ThemeProps, withTheme, styled} from "../App/Theme"
 
-interface CartProductProps {
-  product: CartProduct
-  removeProductFromCart(product: CartProduct): void
-  addProductToCart(product: CartProduct): void
-}
-
-const handleAddProduct = (props: CartProductProps) => () => {
-  props.addProductToCart(props.product)
-}
-
-const handleRemoveProduct = (props: CartProductProps) => () => {
-  props.removeProductFromCart(props.product)
-}
-
 const Button = styled("button")`
   line-height: 1;
   padding: 8px;
@@ -45,114 +31,147 @@ const Option: React.SFC<OptionProps> = props => (
   </div>
 )
 
-const CartProduct: React.StatelessComponent<
-  CartProductProps & ThemeProps
-> = props => {
-  const independentOptions: Array<[string, string]> = []
-  const dependentOptions: Array<[string, string]> = []
-
-  props.product.instance.independentOptions.forEach(independentOption => {
-    props.product.options.forEach(option => {
-      if (option.id === independentOption.optionId) {
-        option.optionValues.forEach(optionValue => {
-          if (optionValue.id === independentOption.valueId) {
-            independentOptions.push([option.name, optionValue.value])
-          }
-        })
-      }
-    })
-  })
-
-  props.product.instance.dependentOptions.forEach(dependentOption => {
-    props.product.options.forEach(option => {
-      if (option.id === dependentOption.optionId) {
-        option.optionValues.forEach(optionValue => {
-          if (optionValue.id === dependentOption.valueId) {
-            dependentOptions.push([option.name, optionValue.value])
-          }
-        })
-      }
-    })
-  })
-
-  return (
-    <div className="row align-items-center mb-4 text-uppercase font-weight-bold">
-      <div className="col-4 col-md-auto my-2 text-center">
-        <img
-          className={cx(
-            css`
-              max-width: 100%;
-              max-height: 64px;
-            `
-          )}
-          src={`https://picsum.photos/300/200/?blue&image=${props.product.id}`}
-        />
-      </div>
-
-      <div className="col-8 col-md mb-2 mb-md-0">
-        <div>{props.product.name}</div>
-        <div
-          className={css`
-            display: flex;
-            flex-flow: row wrap;
-            color: ${props.theme.lightGreen};
-          `}
-        >
-          {independentOptions.map(([name, value], index) => (
-            <Option key={index} name={name} value={value} />
-          ))}
-
-          {dependentOptions.map(([name, value], index) => (
-            <Option key={index} name={name} value={value} />
-          ))}
-        </div>
-      </div>
-
-      <div className="col-4 col-md-2 h4 mb-0 text-center">
-        {props.product.price.value}&nbsp;<small>
-          {props.product.price.currency}
-        </small>
-      </div>
-
-      <div className="col-6 col-md-3">
-        <div className="row no-gutters align-items-center text-center">
-          <div className={"col"}>
-            <Button
-              className="btn btn-outline-danger"
-              onClick={handleRemoveProduct(props)}
-            >
-              <Icon name="minus" />
-            </Button>
-          </div>
-
-          <div className="col">
-            <div
-              className={css`
-                min-width: 2em;
-              `}
-            >
-              <span className="h4 mb-0">{props.product.count}</span>
-            </div>
-          </div>
-
-          <div className="col">
-            <Button
-              className="btn btn-outline-success"
-              onClick={handleAddProduct(props)}
-            >
-              <Icon name="plus" />
-            </Button>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-2 col-md-1 text-center">
-        <span className="h5 mb-0 text-danger">
-          <Icon name="times" />
-        </span>
-      </div>
-    </div>
-  )
+interface CartProductProps {
+  product: CartProduct
+  removeProductFromCart(product: CartProduct): void
+  addProductToCart(product: CartProduct): void
 }
 
-export default withTheme(CartProduct)
+interface CartProductState {
+  independentOptions: Array<[string, string]>
+  dependentOptions: Array<[string, string]>
+}
+
+class CartProductView extends React.Component<
+  CartProductProps & ThemeProps,
+  CartProductState
+> {
+  constructor(props: CartProductProps & ThemeProps) {
+    super(props)
+
+    this.state = {
+      independentOptions: [],
+      dependentOptions: [],
+    }
+
+    props.product.instance.independentOptions.forEach(independentOption => {
+      props.product.options.forEach(option => {
+        if (option.id === independentOption.optionId) {
+          option.optionValues.forEach(optionValue => {
+            if (optionValue.id === independentOption.valueId) {
+              this.state.independentOptions.push([
+                option.name,
+                optionValue.value,
+              ])
+            }
+          })
+        }
+      })
+    })
+
+    props.product.instance.dependentOptions.forEach(dependentOption => {
+      props.product.options.forEach(option => {
+        if (option.id === dependentOption.optionId) {
+          option.optionValues.forEach(optionValue => {
+            if (optionValue.id === dependentOption.valueId) {
+              this.state.dependentOptions.push([option.name, optionValue.value])
+            }
+          })
+        }
+      })
+    })
+  }
+
+  handleRemoveProduct = () => {
+    this.props.removeProductFromCart(this.props.product)
+  }
+
+  handleAddProduct = () => {
+    this.props.addProductToCart(this.props.product)
+  }
+
+  render() {
+    return (
+      <div className="row align-items-center mb-4 text-uppercase font-weight-bold">
+        <div className="col-4 col-md-auto my-2 text-center">
+          <img
+            className={cx(
+              css`
+                max-width: 100%;
+                max-height: 64px;
+              `
+            )}
+            src={`https://picsum.photos/300/200/?blue&image=${
+              this.props.product.id
+            }`}
+          />
+        </div>
+
+        <div className="col-8 col-md mb-2 mb-md-0">
+          <div>{this.props.product.name}</div>
+          <div
+            className={css`
+              display: flex;
+              flex-flow: row wrap;
+              color: ${this.props.theme.lightGreen};
+            `}
+          >
+            {this.state.independentOptions.map(([name, value], index) => (
+              <Option key={index} name={name} value={value} />
+            ))}
+
+            {this.state.dependentOptions.map(([name, value], index) => (
+              <Option key={index} name={name} value={value} />
+            ))}
+          </div>
+        </div>
+
+        <div className="col-4 col-md-2 h4 mb-0 text-center">
+          {this.props.product.price.value}&nbsp;<small>
+            {this.props.product.price.currency}
+          </small>
+        </div>
+
+        <div className="col-6 col-md-3">
+          <div className="row no-gutters align-items-center text-center">
+            <div className={"col"}>
+              <Button
+                className="btn btn-outline-danger"
+                onClick={this.handleRemoveProduct}
+              >
+                <Icon name="minus" />
+              </Button>
+            </div>
+
+            <div className="col">
+              <div
+                className={css`
+                  min-width: 2em;
+                `}
+              >
+                <span className="h4 mb-0">{this.props.product.count}</span>
+              </div>
+            </div>
+
+            <div className="col">
+              <Button
+                className="btn btn-outline-success"
+                onClick={this.handleAddProduct}
+              >
+                <Icon name="plus" />
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-2 col-md-1 text-center">
+          <span className="h5 mb-0 text-danger">
+            <Icon name="times" />
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withTheme(CartProductView)
