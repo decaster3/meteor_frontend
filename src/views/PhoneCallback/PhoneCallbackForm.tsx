6fac5a34998511f582c463cf580f3b9135e7ed9b
bulnab <bulnab@gmail.com>
2018-07-15 23:29:07 +0300

    Make TSLint happy

--- src/views/PhoneCallback/PhoneCallbackForm.tsx
+++ src/views/PhoneCallback/PhoneCallbackForm.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
-import {reduxForm} from "redux-form/immutable"
-import {Field} from "redux-form/immutable"
+import {reduxForm, Field} from "redux-form/immutable"
 import {PulseLoader} from "react-spinners"
 import {InjectedFormProps} from "redux-form"
 import {Map as ImmutableMap} from "immutable"
