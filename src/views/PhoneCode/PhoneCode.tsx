73006ff25fd393d6063aab271c77e41eb4ba6d35
bulnab <bulnab@gmail.com>
2018-08-26 19:31:34 +0300

    Refactor phone code modal

--- src/views/PhoneCode/PhoneCode.tsx
+++ src/views/PhoneCode/PhoneCode.tsx
@@ -46,25 +46,6 @@ class CodeForm extends React.Component<CodeFormProps, CodeFormState> {
   }
 
   render() {
-    const sentCode =
-      moment().diff(moment(this.props.codeSent), "seconds") - 60 < 0 ? (
-        <div className="col">
-          Номер можно изменить через {this.state.counter}
-        </div>
-      ) : (
-        <div className="form-group row">
-          <div className="row">
-            <div className="col">
-              <button
-                onClick={this.props.handleReSendPhone}
-                className="btn btn-block btn-default"
-              >
-                Изменить номер
-              </button>
-            </div>
-          </div>
-        </div>
-      )
     return (
       <div>
         <div className={styles.modalTitle}>
@@ -72,12 +53,25 @@ class CodeForm extends React.Component<CodeFormProps, CodeFormState> {
             Регистрация шаг 2/2
           </h4>
         </div>
+
         <PhoneCodeForm
           onSubmit={this.handleCodeSubmit}
           isCodePending={this.props.isCodePending}
           initialValues={fromJS({phone: this.props.phone})}
         />
-        {sentCode}
+
+        <div className="mt-3">
+          {moment().diff(moment(this.props.codeSent), "seconds") - 60 < 0 ? (
+            `Номер можно изменить через ${this.state.counter}`
+          ) : (
+            <button
+              onClick={this.props.handleReSendPhone}
+              className="btn btn-block btn-outline-success"
+            >
+              Изменить номер
+            </button>
+          )}
+        </div>
       </div>
     )
   }
